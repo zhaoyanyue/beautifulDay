9b0d344f6f6c7d471c96464a3cf4c8f30347aa67
zhaoyanyue <964460565@qq.com>
2019-10-28 15:44:11 +0800

    添加Good类

---
+++ src/main/java/com/test/Good.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author ZYY
+ * @date 2019/10/28
+ */
+public class Good {
+    public static void main(String[] args) {
+        System.out.println("Today is a nice day");
+    }
+}
