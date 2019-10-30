614469f5a4da40dc94bf9cc0c24b62a1ce4bb01a
zhaoyanyue <964460565@qq.com>
2019-10-30 14:54:06 +0800

    添加Good类

--- src/main/java/com/test/Good.java
+++ src/main/java/com/test/Good.java
@@ -8,5 +8,6 @@ public class Good {
     public static void main(String[] args) {
         System.out.println("Today is a nice day");
         System.out.println("good good study,day day up ");
+
     }
 }
